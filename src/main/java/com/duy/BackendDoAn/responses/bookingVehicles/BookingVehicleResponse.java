6b9332d70abdfdba999a7f2a9f12a97e3fe7835d
ddthumonky88 <dothutsk2k2@gmail.com>
2024-12-18 00:43:06 +0700

    update 12.18

--- src/main/java/com/duy/BackendDoAn/responses/bookingVehicles/BookingVehicleResponse.java
+++ src/main/java/com/duy/BackendDoAn/responses/bookingVehicles/BookingVehicleResponse.java
@@ -11,6 +11,7 @@ import lombok.*;
 @NoArgsConstructor
 @Builder
 public class BookingVehicleResponse {
+    private String id;
     private Long user;
     private VehicleResponse vehicle;
     private FacilityResponse facility;
@@ -33,6 +34,7 @@ public class BookingVehicleResponse {
     public static BookingVehicleResponse fromBooking(BookingVehicle bookingVehicle) {
         if (bookingVehicle != null) {
             return BookingVehicleResponse.builder()
+                    .id(bookingVehicle.getId())
                     .user(bookingVehicle.getUser().getId())
                     .customerResponse(CustomerResponse.fromBooking(bookingVehicle))
                     .vehicle(
